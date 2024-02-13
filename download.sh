d850da3176b7dec5a45d22059abd162cb6805f13
sudorook <daemon@nullcodon.com>
2024-02-13 16:10:09 -0600

    add dl script

---
+++ download.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+set -euo pipefail
+
+BASEURL=https://ftp.ebi.ac.uk/pub/databases/Pfam/current_release
+FILES=(pfamseq.{txt,sql}.gz)
+
+function get_pfam_current_release {
+  curl -s "${BASEURL}/relnotes.txt" |
+    sed -n '2s/\s\+RELEASE\s\(.*\)\s*/\1/p'
+}
+
+# Get current release
+VERSION="$(get_pfam_current_release)"
+if [ -z "${VERSION}" ]; then
+  echo Failed to get latest Pfam release version number. Exiting.
+  exit 3
+fi
+
+mkdir -p "pfam_${VERSION}"
+pushd "pfam_${VERSION}" > /dev/null
+
+curl -O -C - "${BASEURL}/userman.txt"
+for FILE in "${FILES[@]}"; do
+  curl -O -C - "${BASEURL}/database_files/${FILE}"
+done
+
+popd > /dev/null
